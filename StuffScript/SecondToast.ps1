cd0b964388823f4567d59da1a7bb3630a4b90edf
AppleSang <96937073+AppleSang@users.noreply.github.com>
2024-10-24 21:17:48 +0700

    Update SecondToast.ps1

--- StuffScript/SecondToast.ps1
+++ StuffScript/SecondToast.ps1
@@ -1,5 +1,5 @@
 $xml = @"
-<toast launch="action=viewFriendRequest&amp;userId=49183", activationType="protocol" launch="https://www.google.com/">
+<toast launch="action=viewFriendRequest&amp;userId=49183">
   <visual>
     <binding template="ToastGeneric">
       <text>=-Welcome To AppleAutoServer-=</text>
