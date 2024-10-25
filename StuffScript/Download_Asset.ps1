69c1b1409cea7d91e37d615cf686f8c971534654
AppleSang <96937073+AppleSang@users.noreply.github.com>
2024-10-25 08:18:47 +0700

    Update Download_Asset.ps1

--- StuffScript/Download_Asset.ps1
+++ StuffScript/Download_Asset.ps1
@@ -1,1 +1,5 @@
-
+New-Item -Path "$env:temp" -Name "AppleAsset" -ItemType "directory"
+cd $env:temp\AppleAsset
+Invoke-WebRequest -Uri 'https://media.tenor.com/MviKulS1NP0AAAAM/minecraft-microsoft.gif' -OutFile 'frog.gif'
+Invoke-WebRequest -Uri 'https://media.tenor.com/rXMWQ5ng9t0AAAAi/golden-apple-minecraft.gif' -OutFile 'apple.gif'
+Invoke-WebRequest -Uri 'https://media2.giphy.com/media/vsREO0xnzFGEuCH2Nl/200w.gif' -OutFile 'look.gif'
