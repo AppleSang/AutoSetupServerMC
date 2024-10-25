7e4833dac3918f639b26cf926a7766ea97f70bcd
AppleSang <96937073+AppleSang@users.noreply.github.com>
2024-10-25 22:41:39 +0700

    Update MC_Paper.ps1

--- StuffScript/MC_Paper.ps1
+++ StuffScript/MC_Paper.ps1
@@ -1,5 +1,5 @@
 # Define the API URL
-$apiUrl = "https://api.papermc.io/v2/projects/paper"
+$apiUrl = "https://api.papermc.io/v2/projects/$proj"
 
 # Fetch the JSON data from the API
 $response = Invoke-RestMethod -Uri $apiUrl
@@ -15,16 +15,16 @@ $versionIndex = Read-Host "Hãy Nhập Số Thứ Tự Để Chọn Phiên Bản
 $selectedVersion = $response.versions[$versionIndex - 1]
 
 # Fetch builds for the selected version
-$buildsUrl = "https://api.papermc.io/v2/projects/paper/versions/$selectedVersion/builds"
+$buildsUrl = "https://api.papermc.io/v2/projects/$proj/versions/$selectedVersion/builds"
 $buildsResponse = Invoke-RestMethod -Uri $buildsUrl
 
 # Get the latest build number (only the "build" value)
 $latestBuild = $buildsResponse.builds[-1].build
 
 # Construct the download URL using the latest build number
-$downloadUrl = "https://api.papermc.io/v2/projects/paper/versions/$selectedVersion/builds/$latestBuild/downloads/paper-$selectedVersion-$latestBuild.jar"
+$downloadUrl = "https://api.papermc.io/v2/projects/$proj/versions/$selectedVersion/builds/$latestBuild/downloads/$proj-$selectedVersion-$latestBuild.jar"
 
 # Download the paper.jar file
 Invoke-WebRequest -Uri $downloadUrl -OutFile "server.jar"
 
-Write-Host "Bạn Đã Tải $selectedVersion, Với Bản Build $latestBuild"
+Write-Host "Bạn Đã Tải $proj Với Phiên $selectedVersion, Bản Build $latestBuild"
